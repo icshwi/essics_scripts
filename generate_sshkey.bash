7338a826c80a783889ea71c14adc7a09f357351a
Jeong Han Lee <jeonghan.lee@gmail.com>
2018-05-08 19:14:19 +0200

    add display for the generated key

--- generate_sshkey.bash
+++ generate_sshkey.bash
@@ -5,3 +5,10 @@ host_name=${HOSTNAME}
 
 ssh-keygen -t rsa -b 4096 -C "${user_name}@${host_name}"
 
+echo ""
+echo " ---- snip snip ---"
+echo ""
+cat ${HOME}/.ssh/id_rsa.pub
+echo ""
+echo " ---- snip snip ---"
+echo ""
